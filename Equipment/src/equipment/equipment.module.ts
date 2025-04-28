7650274d42536dc83242318f7857779c6c683201
Egor <kerminegor@gmail.com>
2025-04-28 18:18:36 +0300

    report init

--- Equipment/src/equipment/equipment.module.ts
+++ Equipment/src/equipment/equipment.module.ts
@@ -1,12 +1,8 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { EquipmentService } from './equipment.service';
 import { EquipmentController } from './equipment.controller';
-import { FilesModule } from '../files/files.module';
 
 @Module({
-  imports: [
-    forwardRef(() => FilesModule), 
-  ],
   controllers: [EquipmentController],
   providers: [EquipmentService],
   exports: [EquipmentService], 
